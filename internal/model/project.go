722b244af2ba394936aa25f75b9ad7cf9c16b4b2
denizgursoy <denizgursoy.443@gmail.com>
2022-08-24 21:41:27 +0200

    [#4] add validation to project structure

--- internal/model/project.go
+++ internal/model/project.go
@@ -1,15 +1,64 @@
 package model
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+)
 
 type (
 	ProjectStructureData struct {
-		Name      string `yaml:"name"`
-		Reference string `yaml:"reference"`
-		URL       string `yaml:"url"`
+		Name      string      `yaml:"name"`
+		Reference string      `yaml:"reference"`
+		URL       string      `yaml:"url"`
+		Questions []*Question `yaml:"Questions"`
+	}
+
+	Question struct {
+		Direction         string    `yaml:"Direction"`
+		CanSkip           bool      `yaml:"CanSkip"`
+		CanSelectMultiple bool      `yaml:"CanSelectMultiple"`
+		Options           []*Option `yaml:"options"`
+	}
+
+	Option struct {
+		Answer       string    `yaml:"Answer"`
+		Dependencies []*string `yaml:"Dependencies"`
+		Files        []*File   `yaml:"Files"`
+	}
+
+	File struct {
+		Url     string `yaml:"Url"`
+		Content string `yaml:"Content"`
+		Path    string `yaml:"Path"`
 	}
 )
 
 func (p *ProjectStructureData) String() string {
 	return fmt.Sprintf("%s (%s)", p.Name, p.Reference)
+}
+
+var (
+	ErrProjectURLIsEmpty    = errors.New("project url can not be empty")
+	ErrProjectNameIsEmpty   = errors.New("project name can not be empty")
+	ErrProjectURLIsNotValid = errors.New("project name can not be empty")
+)
+
+func (p *ProjectStructureData) IsValid() error {
+	if len(strings.TrimSpace(p.Name)) == 0 {
+		return ErrProjectNameIsEmpty
+	}
+
+	projectUrl := strings.TrimSpace(p.URL)
+	if len(projectUrl) == 0 {
+		return ErrProjectURLIsEmpty
+	}
+
+	if _, err := url.ParseRequestURI(projectUrl); err != nil {
+		return ErrProjectURLIsNotValid
+	}
+
+	return nil
+
 }
---
+++ internal/model/project_test.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProjectStructureData_IsValid(t *testing.T) {
+	projectName := "test-Project"
+	validProjectURL := "https://github.com/denizgursoy/gotouch/graphs/traffic"
+
+	validProjectWithNoCustom := ProjectStructureData{
+		Name:      projectName,
+		Reference: "",
+		URL:       validProjectURL,
+	}
+
+	t.Run("should return ErrProjectNameIsEmpty if url is empty", func(t *testing.T) {
+		project := &ProjectStructureData{
+			Name:      "",
+			Reference: "",
+			URL:       "",
+		}
+		err := project.IsValid()
+
+		require.NotNil(t, err)
+		require.ErrorIs(t, ErrProjectNameIsEmpty, err)
+	})
+
+	t.Run("should return ErrProjectURLIsEmpty if url is empty", func(t *testing.T) {
+		project := &ProjectStructureData{
+			Name:      projectName,
+			Reference: "",
+			URL:       "",
+		}
+		err := project.IsValid()
+
+		require.NotNil(t, err)
+		require.ErrorIs(t, ErrProjectURLIsEmpty, err)
+	})
+
+	t.Run("should return ErrProjectURLIsNotValid if url is not valid", func(t *testing.T) {
+		project := &ProjectStructureData{
+			Name:      projectName,
+			Reference: "",
+			URL:       "test-url",
+		}
+		err := project.IsValid()
+
+		require.NotNil(t, err)
+		require.ErrorIs(t, ErrProjectURLIsNotValid, err)
+	})
+
+	t.Run("should return no error", func(t *testing.T) {
+		err := validProjectWithNoCustom.IsValid()
+		require.Nil(t, err)
+	})
+
+}
