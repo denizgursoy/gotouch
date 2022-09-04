1e3fcd8d5c45e73fe64e253d727e04e9f506e146
denizgursoy <denizgursoy.443@gmail.com>
2022-09-04 12:22:51 +0200

    [#4] add test to compress directory

---
+++ internal/commander/packageCommand_test.go
@@ -0,0 +1,52 @@
+package commander
+
+import (
+	"github.com/denizgursoy/gotouch/internal/compressor"
+	"github.com/denizgursoy/gotouch/internal/logger"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_cmdExecutor_CompressDirectory(t *testing.T) {
+	t.Run("should call compress directory on the executor", func(t *testing.T) {
+		instance := GetInstance()
+
+		controller := gomock.NewController(t)
+		mockCompressor := compressor.NewMockCompressor(controller)
+
+		sourceDirectory := "sourcedir"
+
+		opts := &PackageCommandOptions{
+			SourceDirectory: &sourceDirectory,
+			TargetDirectory: nil,
+			Compressor:      mockCompressor,
+			Logger:          logger.NewLogger(),
+		}
+		mockCompressor.EXPECT().CompressDirectory(gomock.Eq(sourceDirectory), gomock.Eq(""))
+
+		err := instance.CompressDirectory(opts)
+		require.Nil(t, err)
+	})
+
+	t.Run("should call compress directory on the executor with target", func(t *testing.T) {
+		instance := GetInstance()
+
+		controller := gomock.NewController(t)
+		mockCompressor := compressor.NewMockCompressor(controller)
+
+		sourceDirectory := "sourcedir"
+		targetDirectory := "targetdir"
+
+		opts := &PackageCommandOptions{
+			SourceDirectory: &sourceDirectory,
+			TargetDirectory: &targetDirectory,
+			Compressor:      mockCompressor,
+			Logger:          logger.NewLogger(),
+		}
+		mockCompressor.EXPECT().CompressDirectory(gomock.Eq(sourceDirectory), gomock.Eq(targetDirectory))
+
+		err := instance.CompressDirectory(opts)
+		require.Nil(t, err)
+	})
+}
