c0159fe7e6f923af073d22bb67f72b74bdbf57e2
denizgursoy <denizgursoy.443@gmail.com>
2022-10-02 12:12:42 +0200

    [#4] test dependency

---
+++ internal/manager/dependency_test.go
@@ -0,0 +1,16 @@
+package manager
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDisplayName(t *testing.T) {
+	t.Run("should include reference if exists", func(t *testing.T) {
+		version := "1234"
+		url := "*"
+		dependency := Dependency{Url: &url, Version: &version}
+		require.EqualValues(t, url+"@"+version, dependency.String())
+	})
+}
