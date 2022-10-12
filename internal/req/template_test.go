d0a8d01dc678a65b6c3ab24abf38924846ac5a9b
denizgursoy <denizgursoy.443@gmail.com>
2022-10-12 22:36:07 +0200

    [#4] add template test

---
+++ internal/req/template_test.go
@@ -0,0 +1,33 @@
+package req
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/denizgursoy/gotouch/internal/prompter"
+	"github.com/denizgursoy/gotouch/internal/store"
+	"github.com/golang/mock/gomock"
+)
+
+func TestAskForInput(t *testing.T) {
+	t.Run("should not prompt to user if values are empty", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockPrompter := prompter.NewMockPrompter(controller)
+		mockStore := store.GetInstance()
+
+		requirement := templateRequirement{
+			Prompter: mockPrompter,
+			Store:    mockStore,
+			Values:   nil,
+		}
+
+		input, requirements, err := requirement.AskForInput()
+		require.Nil(t, err)
+		require.Nil(t, requirements)
+		require.Len(t, input, 1)
+		require.IsType(t, input[0], &templateTask{})
+	})
+}
