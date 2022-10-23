90445f202ce8cdffaceba97a28949de73c02ef73
denizgursoy <denizgursoy.443@gmail.com>
2022-10-23 13:33:14 +0200

    [#4] add validate tests

---
+++ internal/root/validateCommand_test.go
@@ -0,0 +1,37 @@
+package root
+
+import (
+	"github.com/denizgursoy/gotouch/internal/lister"
+	"github.com/denizgursoy/gotouch/internal/logger"
+	"github.com/denizgursoy/gotouch/internal/operator"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetValidateCommandHandler(t *testing.T) {
+	t.Run("should validate successfully", func(t *testing.T) {
+
+		controller := gomock.NewController(t)
+		mockCommander := operator.NewMockOperator(controller)
+
+		path := "sdsd"
+		expectedCall := &operator.ValidateYamlOptions{
+			Lister: lister.GetInstance(),
+			Logger: logger.NewLogger(),
+			Path:   &path,
+		}
+
+		mockCommander.EXPECT().ValidateYaml(gomock.Eq(expectedCall))
+
+		command := CreateValidateCommand(mockCommander)
+
+		command.SetArgs(getValidateTestArguments(path))
+		err := command.Execute()
+		require.Nil(t, err)
+	})
+}
+
+func getValidateTestArguments(source string) []string {
+	return []string{"-f", source}
+}
