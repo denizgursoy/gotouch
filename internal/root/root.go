e95d709cf2bff2d6e388e0521a7f4daa335123ae
denizgursoy <denizgursoy.443@gmail.com>
2022-10-28 21:13:32 +0200

    [#4] add command descriptions

--- internal/root/root.go
+++ internal/root/root.go
@@ -18,11 +18,22 @@ func Execute() {
 func CreateRootCommand(cmdr operator.Operator) *cobra.Command {
 	createCommand := &cobra.Command{
 		Use:   "gotouch",
-		Short: "Helps you create new Go Projects",
-		Long:  `Tag`,
-		Run:   GetCreateCommandHandler(cmdr),
+		Short: "Helps you create new  projects",
+		Long: `Parses properties yaml provided with file flag. If no flag is provided, Gotouch will use the yaml at
+https://raw.githubusercontent.com/denizgursoy/go-touch-projects/main/package.yaml. 
+Gotouch will list the project structures in the yaml to user in order to make selection.
+Gotouch will ask for project name. Project name will be used to create directory to which archive file will be extracted.
+If selected project structure's language is go, project name will be used as module name. See below:
+
+Project Name						Module Name 						Directory Name
+my-app								my-app								my-app
+github.com/my-account/my-app		github.com/my-account/my-app		my-app
+
+If no go.mod file exists in the template, Gotouch will create one with the module name.
+Gotouch will ask the questions in the selected project structure in order.`,
+		Run: GetCreateCommandHandler(cmdr),
 	}
-	createCommand.Flags().StringP(FileFlagName, "f", "", "input file")
+	createCommand.Flags().StringP(FileFlagName, "f", "", "input properties yaml")
 
 	createCommand.AddCommand(CreatePackageCommand(cmdr))
 	createCommand.AddCommand(CreateValidateCommand(cmdr))
@@ -33,9 +44,18 @@ func CreateRootCommand(cmdr operator.Operator) *cobra.Command {
 func CreatePackageCommand(cmdr operator.Operator) *cobra.Command {
 	packageCommand := &cobra.Command{
 		Use:   "package",
-		Short: "createYourZip",
-		Long:  `Tag`,
-		Run:   GetPackageCommandHandler(cmdr),
+		Short: "Create archive file from directory compatible with gotouch",
+		Long: `Creates a tar file with gzip compression. Ignores following files/directories:
+
+__MACOS
+.DS_Store
+.idea
+.vscode
+.git
+
+If no flag is not set, package command creates archive file in the parent directory 
+with .tar.gz extension from the files/directories in the working dir.`,
+		Run: GetPackageCommandHandler(cmdr),
 	}
 
 	packageCommand.Flags().StringP(SourceDirectoryFlagName, "s", ".", "source directory")
@@ -48,11 +68,12 @@ func CreateValidateCommand(cmdr operator.Operator) *cobra.Command {
 	validateCommand := &cobra.Command{
 		Use:   "validate",
 		Short: "Validation Check for YAML files",
-		Long:  `Tag`,
-		Run:   GetValidateCommandHandler(cmdr),
+		Long: `Checks if properties yaml can be used by gotouch.
+See https://raw.githubusercontent.com/denizgursoy/gotouch/main/examples/complete-choice-example.yaml as a guide.`,
+		Run: GetValidateCommandHandler(cmdr),
 	}
 
-	validateCommand.Flags().StringP(FileFlagName, "f", "", "input file")
+	validateCommand.Flags().StringP(FileFlagName, "f", "", "input properties yaml")
 
 	return validateCommand
 }
