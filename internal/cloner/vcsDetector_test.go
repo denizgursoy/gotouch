2a0f2b0f6ccbd0adf4afcf89bc521c494bd8ff00
denizgursoy <denizgursoy.443@gmail.com>
2025-08-06 09:50:35 +0200

    add ssh validation

--- internal/cloner/vcsDetector_test.go
+++ internal/cloner/vcsDetector_test.go
@@ -13,16 +13,10 @@ func Test_defaultVCSDetector_DetectVCS(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, VCSGit, vcs)
 	})
-	t.Run("should return git it it is with ssh url", func(t *testing.T) {
-		detector := NewDefaultVCSDetector()
-		vcs, err := detector.DetectVCS(t.Context(), "git@github.com:denizgursoy/gotouch.git")
-		require.NoError(t, err)
-		require.Equal(t, VCSGit, vcs)
-	})
 	t.Run("should return none it it is git repository", func(t *testing.T) {
 		detector := NewDefaultVCSDetector()
 		vcs, err := detector.DetectVCS(t.Context(), "https://github.com/denizgursoy")
-		require.NoError(t, err)
+		require.Error(t, err)
 		require.Equal(t, VCSNone, vcs)
 	})
 }
